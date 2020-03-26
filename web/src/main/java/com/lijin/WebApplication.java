f07b8a159fd1f19a649652a7ef9c9552b62b2f8b
milotree <1450258563@qq.com>
2020-03-26 21:37:25 +0800

    前端

--- web/src/main/java/com/lijin/WebApplication.java
+++ web/src/main/java/com/lijin/WebApplication.java
@@ -2,8 +2,11 @@ package com.lijin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
+
 public class WebApplication {
 
     public static void main(String[] args) {
--- web/src/test/java/com/lijin/web/WebApplicationTests.java
+++ web/src/test/java/com/lijin/web/WebApplicationTests.java
@@ -2,10 +2,17 @@ package com.lijin.web;
 
 
 import com.lijin.dao.ManagerDao;
+import com.lijin.dao.UserDao;
 import com.lijin.entity.Manager;
+import com.lijin.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.*;
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 @SpringBootTest
 class WebApplicationTests {
@@ -13,12 +20,34 @@ class WebApplicationTests {
     @Autowired
     private ManagerDao managerDao;
 
+    @Autowired
+    private UserDao userDao;
+
     @Test
+    @Transactional
     void contextLoads() {
-        Manager manager = new Manager();
-        manager.setMname("milomilo");
-        manager.setMpass("123456");
-        managerDao.save(manager);
+
+
+        /**
+         * 查询是否有相同的电话号码
+         */
+        Specification<User> spec = new Specification<User>() {
+                @Override
+                public Predicate toPredicate(Root<User> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                    //1.获取比较的属性
+                    Path<Object> userTel = root.get("utel");
+                    //2.构造条查询       ：  select * from pet_user where tel = '***'
+                    return cb.equal(userTel, "13875");//进行精准匹配（比较的属性名，比较的属性取值）
+                }
+            };
+//           Optional<User> one = userDao.findOne(spec);
+        Optional<User> one = userDao.findOne(spec);
+        System.out.println(one);
+            if (one.isPresent()) {
+                System.out.println("有相同的");
+            } else {
+                System.out.println("没有相同的");
+        }
     }
 
     /**
